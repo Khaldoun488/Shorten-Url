063b1afe44370e91baa8d647dd2853b9b9757b8d
Khaldoun488 <khaldoun@iron-mail.net>
2015-04-21 11:33:07 +0200

    add index

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+require('vendor/autoload.php');
+require('config.php');
+
+use Core\Container\ContainerBuilder;
+
+use Symfony\Component\HttpFoundation\Request;
+
+//build container
+$containerBuilder = new ContainerBuilder($parameters);
+$container        = $containerBuilder->init();
+
+//Request
+$request = Request::createFromGlobals();
+$input   = $request->query->get('long_url', null);
+
+$shortUrl = null;
+
+//init smarty
+$smarty = new Smarty();
+
+//call the service
+if ($input !== null) {
+    $shortenUrlService = $container['shorten_url_service'];
+    $shortUrl          = $shortenUrlService->shorten($input);
+
+    $smarty->assign("long_url", $input);
+}
+
+//assign result
+if ($shortUrl !== null) {
+    $smarty->assign("short_url", $shortUrl);
+}
+
+//render
+$smarty->display('view.tpl');
+
